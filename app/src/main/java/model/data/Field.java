ecfde85ade856b4232ea7d66550e2bfa3c092fd0
Rhathin <Rhathin@gmail.com>
2019-12-08 20:54:20 +0100

    Change type of cycle field in Field class

--- app/src/main/java/model/data/Field.java
+++ app/src/main/java/model/data/Field.java
@@ -3,16 +3,16 @@ package model.data;
 public class Field
 {
     private int id;
+    private int cycle;
     private String name;
-    private String cycle;
     private Faculty faculty;
     private Model model;
 
-    public Field(int id, String name, String cycle, Faculty faculty, Model model)
+    public Field(int id, String name, int cycle, Faculty faculty, Model model)
     {
         this.id = id;
-        this.name = name;
         this.cycle = cycle;
+        this.name = name;
         this.faculty = faculty;
         this.model = model;
     }
@@ -27,7 +27,7 @@ public class Field
         return name;
     }
 
-    public String getCycle()
+    public int getCycle()
     {
         return cycle;
     }
