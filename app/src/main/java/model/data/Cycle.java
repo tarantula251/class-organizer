27dbc17a8c2fa3e23a29fc82e37d9135c85004bc
Rhathin <Rhathin@gmail.com>
2019-12-08 21:17:28 +0100

    Add Cycle class

---
+++ app/src/main/java/model/data/Cycle.java
@@ -0,0 +1,23 @@
+package model.data;
+
+public class Cycle
+{
+    private int id;
+    private String name;
+
+    public Cycle(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+}
