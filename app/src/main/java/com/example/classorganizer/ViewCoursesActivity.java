bf33b986a21b448250e95730b674f72badcfbd63
tarantula251 <justyna.zofia.gasior@gmail.com>
2019-12-26 15:07:41 +0100

    Modify binding data in the view courses activity

--- app/src/main/java/com/example/classorganizer/ViewCoursesActivity.java
+++ app/src/main/java/com/example/classorganizer/ViewCoursesActivity.java
@@ -7,30 +7,91 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ViewCoursesActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ViewCoursesAdapter adapter;
-    private ArrayList<String> items;
+    private ArrayList<HashMap<String, ArrayList<String>>> courseList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_courses);
-
+        courseList = new ArrayList<>();
         populateDataList();
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new ViewCoursesAdapter(this, items);
+        adapter = new ViewCoursesAdapter(this, courseList);
         recyclerView.setAdapter(adapter);
     }
 
     private void populateDataList() {
-        items = new ArrayList<>();
-        items.add("First Card View Item");
-        items.add("Scnd Card View Item");
-        items.add("Thrd Card View Item");
-        items.add("Frth Card View Item");
-        items.add("Fth Card View Item");
+        HashMap<String, ArrayList<String>> courseData = new HashMap<>();
+
+        ArrayList<String> course = new ArrayList<>();
+        course.add("Image Processing");
+        courseData.put(getString(R.string.course), course);
+
+        ArrayList<String> cycle = new ArrayList<>();
+        cycle.add("2nd");
+        courseData.put(getString(R.string.cycle), cycle);
+
+        ArrayList<String> model = new ArrayList<>();
+        model.add("part-time");
+        courseData.put(getString(R.string.model), model);
+
+        ArrayList<String> faculty = new ArrayList<>();
+        faculty.add("FTIMS");
+        courseData.put(getString(R.string.faculty), faculty);
+
+        ArrayList<String> field = new ArrayList<>();
+        field.add("Information Technology");
+        courseData.put(getString(R.string.field), field);
+
+        ArrayList<String> supervisor = new ArrayList<>();
+        supervisor.add("dr inż. Michał Masecki");
+        courseData.put(getString(R.string.supervisor), supervisor);
+
+        ArrayList<String> classes = new ArrayList<>();
+        classes.add("Laboratory Saturday 11.00 - 12.30");
+        classes.add("Lecture Sunday 9.00 - 10.30");
+        courseData.put(getString(R.string.classes), classes);
+
+        /*-----------------------------------------------------*/
+
+        HashMap<String, ArrayList<String>> courseData2 = new HashMap<>();
+
+        ArrayList<String> course2 = new ArrayList<>();
+        course2.add("Database Administration");
+        courseData2.put(getString(R.string.course), course2);
+
+        ArrayList<String> cycle2 = new ArrayList<>();
+        cycle2.add("1nd");
+        courseData2.put(getString(R.string.cycle), cycle2);
+
+        ArrayList<String> model2 = new ArrayList<>();
+        model2.add("full-time");
+        courseData2.put(getString(R.string.model), model2);
+
+        ArrayList<String> faculty2 = new ArrayList<>();
+        faculty2.add("FTIMS");
+        courseData2.put(getString(R.string.faculty), faculty2);
+
+        ArrayList<String> field2 = new ArrayList<>();
+        field2.add("Information Technology");
+        courseData2.put(getString(R.string.field), field2);
+
+        ArrayList<String> supervisor2 = new ArrayList<>();
+        supervisor2.add("dr inż. Kacper Morawski");
+        courseData2.put(getString(R.string.supervisor), supervisor2);
+
+        ArrayList<String> classes2 = new ArrayList<>();
+        classes2.add("Laboratory Saturday 12.00 - 13.30");
+        classes2.add("Lecture Sunday 17.00 - 18.30");
+        courseData2.put(getString(R.string.classes), classes2);
+
+        courseList.add(courseData);
+        courseList.add(courseData2);
     }
 }
--- app/src/main/java/com/example/classorganizer/ViewCoursesAdapter.java
+++ app/src/main/java/com/example/classorganizer/ViewCoursesAdapter.java
@@ -1,55 +1,113 @@
 package com.example.classorganizer;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.text.TextUtils;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ViewCoursesAdapter extends RecyclerView.Adapter<ViewCoursesAdapter.CourseViewHolder> {
 
     private LayoutInflater layoutInflater;
-    private List<String> data;
+    private ArrayList<HashMap<String, ArrayList<String>>> data;
+    private Context context;
 
-    public ViewCoursesAdapter(Context context, List<String> data) {
+    public ViewCoursesAdapter(Context context, ArrayList<HashMap<String, ArrayList<String>>> data) {
         this.layoutInflater = LayoutInflater.from(context);
         this.data = data;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = layoutInflater.inflate(R.layout.course_item, parent, false);
-        return new CourseViewHolder(view);
+        GridLayout gridLayout = view.findViewById(R.id.courseGrid);
+
+        if (!data.isEmpty()) {
+            for (HashMap<String, ArrayList<String>> courseMap : data) {
+                ArrayList<String> classes = courseMap.get(context.getResources().getString(R.string.classes));
+
+                if (classes.size() > 0) {
+                    int currentLastRowIndex = 5;
+                    int columnIndex = 1;
+                    for (String classToAppend : classes) {
+                        TextView classTextView = new TextView(context);
+                        classTextView.setTag("classesValue" + classes.indexOf(classToAppend));
+                        classTextView.setTextColor(context.getResources().getColor(R.color.list_item_background_color));
+                        classTextView.setTypeface(null, Typeface.BOLD);
+                        classTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12);
+                        classTextView.setEllipsize(TextUtils.TruncateAt.END);
+
+                        GridLayout.LayoutParams layoutParams = new GridLayout.LayoutParams(
+                                GridLayout.spec(currentLastRowIndex, GridLayout.LEFT),
+                                GridLayout.spec(columnIndex, GridLayout.LEFT)
+                        );
+                        layoutParams.width = GridLayout.LayoutParams.WRAP_CONTENT;
+                        layoutParams.height = GridLayout.LayoutParams.WRAP_CONTENT;
+
+                        gridLayout.addView(classTextView, layoutParams);
+                        currentLastRowIndex++;
+                    }
+                }
+            }
+        }
+
+        final int childCount = gridLayout.getChildCount();
+        int classesCount = 0;
+
+        for (int i = 0; i < childCount; i++) {
+            final View child = gridLayout.getChildAt(i);
+            if (child instanceof TextView && String.valueOf(child.getTag()).contains("classesValue")) {
+                classesCount++;
+            }
+        }
+
+        CourseViewHolder holder = new CourseViewHolder(view, classesCount);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
-        String course = data.get(position);
+        HashMap<String, ArrayList<String>> courseData = data.get(position);
+
+        String course = courseData.get(context.getResources().getString(R.string.course)).get(0);
         holder.textCourseName.setText(course);
 
-        String cycle = data.get(position);
+        String cycle = courseData.get(context.getResources().getString(R.string.cycle)).get(0);
         holder.textCycle.setText(cycle);
 
-        String model = data.get(position);
+        String model = courseData.get(context.getResources().getString(R.string.model)).get(0);
         holder.textModel.setText(model);
 
-        String faculty = data.get(position);
+        String faculty = courseData.get(context.getResources().getString(R.string.faculty)).get(0);
         holder.textFaculty.setText(faculty);
 
-        String field = data.get(position);
+        String field = courseData.get(context.getResources().getString(R.string.field)).get(0);
         holder.textField.setText(field);
 
-        String supervisor = data.get(position);
+        String supervisor = courseData.get(context.getResources().getString(R.string.supervisor)).get(0);
         holder.textSupervisor.setText(supervisor);
 
-        String classes = data.get(position);
-        holder.textClasses.setText(classes);
+        ArrayList<String> classes = courseData.get(context.getResources().getString(R.string.classes));
+        holder.classesSize = classes.size();
+
+        if (holder.classesSize > 0) {
+            for (int i = 0; i < holder.classesSize; i++) {
+                String classVal = classes.get(i);
+                holder.textClasses.get(i).setText(classVal);
+            }
+        }
     }
 
     @Override
@@ -58,10 +116,11 @@ public class ViewCoursesAdapter extends RecyclerView.Adapter<ViewCoursesAdapter.
     }
 
     public static class CourseViewHolder extends RecyclerView.ViewHolder {
+        TextView textCourseName, textCycle, textModel, textFaculty, textField, textSupervisor;
+        ArrayList<TextView> textClasses;
+        int classesSize;
 
-        TextView textCourseName, textCycle, textModel, textFaculty, textField, textSupervisor, textClasses; //TODO modify textClasses to hold many classes values
-
-        public CourseViewHolder(@NonNull View itemView) {
+        public CourseViewHolder(@NonNull View itemView, int classesSize) {
             super(itemView);
             textCourseName = itemView.findViewById(R.id.courseName);
             textCycle = itemView.findViewById(R.id.cycleValue);
@@ -69,7 +128,14 @@ public class ViewCoursesAdapter extends RecyclerView.Adapter<ViewCoursesAdapter.
             textFaculty = itemView.findViewById(R.id.facultyValue);
             textField = itemView.findViewById(R.id.fieldValue);
             textSupervisor = itemView.findViewById(R.id.supervisorValue);
-            textClasses = itemView.findViewById(R.id.classesValue);
+
+            this.classesSize = classesSize;
+            textClasses = new ArrayList<>();
+            if (classesSize > 0) {
+                for (int i = 0; i < classesSize; i++) {
+                    textClasses.add((TextView) itemView.findViewWithTag("classesValue" + i));
+                }
+            }
         }
     }
 }
