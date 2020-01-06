5a2fcd094f254af1cec063feed956347297cacc7
Rhathin <Rhathin@gmail.com>
2020-01-06 19:43:39 +0100

    Change FieldJSONBuilder class to use param arrays instead of single values

--- app/src/main/java/model/builders/FieldJSONBuilder.java
+++ app/src/main/java/model/builders/FieldJSONBuilder.java
@@ -3,27 +3,51 @@ package model.builders;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import model.data.Cycle;
+import model.data.DataComparator;
 import model.data.Faculty;
 import model.data.Field;
 import model.data.Model;
 
 public class FieldJSONBuilder implements JSONDataBuilder<Field>
 {
-    private Faculty faculty;
-    private Model model;
-    private Cycle cycle;
+    private ArrayList<Faculty> faculties;
+    private ArrayList<Model> models;
+    private ArrayList<Cycle> cycles;
 
-    public FieldJSONBuilder(Faculty faculty, Model model, Cycle cycle)
+    public FieldJSONBuilder(ArrayList<Faculty> faculties, ArrayList<Model> models, ArrayList<Cycle> cycles)
     {
-        this.faculty = faculty;
-        this.model = model;
-        this.cycle = cycle;
+        this.faculties = faculties;
+        this.models = models;
+        this.cycles = cycles;
     }
 
     @Override
     public Field buildData(JSONObject jsonObject) throws JSONException
     {
+        int facultyId = jsonObject.getInt("faculty");
+        int modelId = jsonObject.getInt("model");
+        int cycleId = jsonObject.getInt("cycle");
+
+
+        DataComparator dataComparator = new DataComparator();
+        Faculty faculty;
+        Model model;
+        Cycle cycle;
+        try
+        {
+            faculty = faculties.get(Collections.binarySearch(faculties, new Faculty(facultyId, ""), dataComparator));
+            model = models.get(Collections.binarySearch(models, new Model(modelId, ""), dataComparator));
+            cycle = cycles.get(Collections.binarySearch(cycles, new Cycle(cycleId, ""), dataComparator));
+        }
+        catch(IndexOutOfBoundsException e)
+        {
+            return null;
+        }
+
         return new Field(jsonObject.getInt("id"),
                 jsonObject.getString("name"),
                 faculty,
